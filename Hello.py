8066e17d6327d5617896a07ead67f7dad086b287
Muhd Danniel Bin Abdullah <mdanniel45@gmail.com>
2023-02-07 16:15:43 +0800

    Semakan pertama

---
+++ Hello.py
@@ -0,0 +1,40 @@
+#Muhd Danniel Bin Abdullah
+#0128591239
+#danniel@edu.pmu.my
+
+#Develop a software to calculate Sales
+# 1)ask name of a user
+# 2)Ask  how many percent is the sales Tax
+# 3)The output :Hi xxxx, the total amount to pay is xxx ,the sales tax is xxx
+# sales ammount = total sales + sales tax
+# barang $1500 tax 6.25% maka nilai keselruhan ialah 1631.25
+
+#name=input("What is your name? \n")
+#tax=float(input("How many percent is the sales Tax? "))
+#total_sales=1500
+#total_tax=total_sales+(total_sales*tax)
+
+
+
+
+#print("Hi "+name+",the total ammount to pay is ")
+
+#name = input("What is your name? ")
+#tax = float(input("How many percent is the sales Tax: "))
+#sale = float(input("How much is the total amount: "))
+#sales_tax = tax/100 * sale
+#sales_amount = sales_tax +sale
+#print("Hello "+name+" your total sales is "+str(sales_amount))
+
+
+#
+#ask for user infut
+#change celsius to fahrenhiet
+
+
+name = input("Please enter your name ")
+celsius=float(input("Please enter temperature in celsius "))
+
+fahrenhiet=celsius*9/5+32
+
+print(name+" the temperature is "+str(fahrenhiet)+"fahrenhiet.")
